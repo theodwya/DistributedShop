8701114ccecf8ac2b8b7388768201c49aba2be0f
theodwya <navyteddy5286@gmail.com>
2024-09-26 14:17:03 -0400

    Development Environment Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to DistributedShop Frontend!</h1>
+        </div>
+    );
+}
+
+export default App;
